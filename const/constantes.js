779d01b4d47b335f7caa6068b80221df1b3b441d
cfranciscoja <e_caravena@hotmail.com>
2024-12-17 12:38:04 -0300

    cambio constantes

---
+++ const/constantes.js
@@ -0,0 +1,17 @@
+function define(name, value) {
+    Object.defineProperty(exports, name, {
+        value:      value,
+        enumerable: true
+    });
+}
+
+define("hostcorreo", "xxx");
+define("portcorreo", 465);
+define("securecorreo", true);
+define("usercorreo", "xxx");
+define("passcorreo", "xxx");
+
+
+
+
+
